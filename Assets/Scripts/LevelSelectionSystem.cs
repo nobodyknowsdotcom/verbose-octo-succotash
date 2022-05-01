0bbfa97988f2f413b59105a77bcf678b9e3bcc5d
Dmitry Medvedev <myclosestisa@gmail.com>
2022-05-01 16:32:41 +0500

    Highlight available levels

--- Assets/Scripts/LevelSelectionSystem.cs
+++ Assets/Scripts/LevelSelectionSystem.cs
@@ -1,19 +1,40 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelSelectionSystem : MonoBehaviour
 {
     
     [SerializeField] public GameObject levelsParent;
-    private static int currentLevel;
+    private static int _currentLevel;
+
+    private static Dictionary<int, List<int>> availableLevels = new Dictionary<int, List<int>>
+    {
+        {0, new List<int>{1, 2, 3}},
+        {1, new List<int>{0, 2, 4}},
+        {2, new List<int>{0, 1, 4}},
+        {3, new List<int>{0, 5}},
+        {4, new List<int>{1, 2, 5, 6}},
+        {5, new List<int>{3, 6}},
+        {6, new List<int>{4, 5}},
+    };
     
     public void Update()
     {
         foreach (Transform level in levelsParent.transform)
         {
-            if (level.name != currentLevel.ToString())
+            if (level.name != _currentLevel.ToString())
             {
                 level.Find("OnActive").gameObject.SetActive(false);
+            }
+            
+            if (availableLevels[_currentLevel].Contains(Int32.Parse(level.name)))
+            {
+                level.Find("OnAvailable").gameObject.SetActive(true);
+            }
+            else
+            {
+                level.Find("OnAvailable").gameObject.SetActive(false);
             }
         }
     }
@@ -26,13 +47,13 @@ public class LevelSelectionSystem : MonoBehaviour
 
     public static int GetCurrentLevel()
     {
-        return currentLevel;
+        return _currentLevel;
     }
     
     public static void SetCurrentLevel(GameObject level)
     {
         var enabledIcon = level.transform.Find("OnActive").gameObject;
         enabledIcon.SetActive(true);
-        currentLevel = Int32.Parse(level.name);
+        _currentLevel = Int32.Parse(level.name);
     }
 }
