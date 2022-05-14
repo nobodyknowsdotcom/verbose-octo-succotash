3c576df629373dd285e909838a74b5d1579ddc49
Dmitry Medvedev <myclosestisa@gmail.com>
2022-05-14 12:53:59 +0500

    Highlight current and target cells on the battle field

--- Assets/Scripts/Battle/FieldManager.cs
+++ Assets/Scripts/Battle/FieldManager.cs
@@ -1,21 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class FieldManager : MonoBehaviour
 {
     [SerializeField] private GameObject cellPrefab;
     [SerializeField] private GameObject cellsField;
-    private GameObject gameGrid;
-    private int width;
-    private int height;
-    private float cellSize = 1f;
+    private GameObject[,] _gameGrid;
+    private string _currentCell;
+    private string _targetCell;
+    private int _width;
+    private int _height;
+    private float cellSize = 1.2f;
 
     public void Awake()
     {
-        width = 8;
-        height = 8;
+        _width = 8;
+        _height = 8;
     }
 
     public void Start()
@@ -31,16 +32,55 @@ public class FieldManager : MonoBehaviour
             return;
         }
 
-        var gameGrid = new GameObject[width, height];
+        _gameGrid = new GameObject[_width, _height];
+        var startPos = cellsField.transform.position + new Vector3(cellSize/2, cellSize/2);
 
-        for (int i = 0; i < height; i++)
+        for (int i = 0; i < _height; i++)
         {
-            for (int j = 0; j < width; j++)
+            for (int j = 0; j < _width; j++)
             {
-                gameGrid[i, j] = Instantiate(cellPrefab, new Vector3(i * cellSize, j * cellSize, 0), Quaternion.identity);
-                gameGrid[i, j].transform.parent = cellsField.transform;
-                gameGrid[i, j].gameObject.name = i + "_" + j;
+                _gameGrid[i, j] = Instantiate(
+                    cellPrefab, 
+                    startPos + new Vector3(i * cellSize, j * cellSize, 0),
+                    Quaternion.identity);
+                _gameGrid[i, j].transform.SetParent(cellsField.transform);
+                _gameGrid[i, j].gameObject.name = i + "_" + j;
+                _gameGrid[i, j].GetComponent<Button>().onClick.AddListener(OnCellCLick);
             }
+        }
+    }
+
+    public void OnCellCLick()
+    {
+        var cell = EventSystem.current.currentSelectedGameObject;
+        // set active targetCell if currentCell exists and not doubleclicked
+        if (_currentCell != null && _currentCell != cell.name)
+        {
+            _targetCell = cell.name;
+        }
+        else
+        {
+            // if currentCell doubleclicked, it will reset current and target cells
+            if (_currentCell == cell.name)
+            {
+                _currentCell = null;
+                _targetCell = null;
+            }
+            else
+            {
+                _currentCell = cell.name;
+            }
+        }
+
+        UpdateGrid();
+    }
+
+    private void UpdateGrid()
+    {
+        foreach (var cell in _gameGrid)
+        {
+            cell.transform.Find("OnActive").gameObject.SetActive(cell.name == _currentCell);
+            cell.transform.Find("OnTarget").gameObject.SetActive(cell.name == _targetCell);
         }
     }
 }
--- Assets/Scripts/Map/LevelManager.cs
+++ Assets/Scripts/Map/LevelManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
